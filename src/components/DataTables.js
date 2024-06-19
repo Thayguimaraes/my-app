b1e80ad6bc31e618f6b42b0205c12798bca72997
thaynara guimaraes <thaynaraelinezdsf@gmail.com>
2024-06-19 17:49:41 -0300

    .

---
+++ src/components/DataTables.js
@@ -0,0 +1,27 @@
+function DataTables(data) {
+    return(
+        <div className="styleDataTable">
+           <table class="table table-dark table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
+        </div>
+    );
+    
+}
+export default DataTables;
