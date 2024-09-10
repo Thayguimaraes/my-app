6f9c2553967180c498541a0ba276b992e747bbac
thaynara guimaraes <thaynaraelinezdsf@gmail.com>
2024-09-10 08:46:28 -0300

    .

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 //components
 import DataTables from './components/DataTables';
-
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-        <h1>teste</h1>
-       <p><DataTables/></p>
+      <Header/>
       </header>
+     
+      
     </div>
   );
 }
